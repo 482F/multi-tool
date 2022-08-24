dc267d256d1cd0c7f023471d43da9510d91f08ed
482F <482F.git.mail@482f.net>
2022-08-25 00:00:00 +0900

    vite-electron-template による初期化

---
+++ src/main/preload/index.js
@@ -0,0 +1,39 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+window.addEventListener('DOMContentLoaded', () => {
+  //
+  // ここに、レンダラープロセスに渡したい機能を記載していく
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
+const sendIpc = async (listenerName, eventName, ...args) => {
+  return JSON.parse(
+    (await ipcRenderer.invoke(
+      'sendIpc',
+      JSON.stringify([`${listenerName}-${eventName}`, args])
+    )) ?? null
+  )
+}
+const listenIpc = async (listenerName, eventName, handler) => {
+  ipcRenderer.on(`${listenerName}-${eventName}`, (_, args) =>
+    handler(...JSON.parse(args))
+  )
+  await sendIpc('main', 'listen', listenerName, eventName)
+}
+
+const passObject = {
+  requires: {
+    listenIpc,
+    sendIpc,
+  },
+}
+
+Object.entries(passObject).forEach(([apiKey, api]) =>
+  contextBridge.exposeInMainWorld(apiKey, api)
+)
