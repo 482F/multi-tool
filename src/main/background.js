ea62bf51af8890736946b5a4d8d86f74faa6de3c
482F <482F.git.mail@482f.net>
2022-08-27 15:30:30 +0900

    vite-electron-template による初期化

---
+++ src/main/background.js
@@ -0,0 +1,187 @@
+import { app, BrowserWindow } from 'electron'
+import utls from './main-utls.js'
+const path = require('path')
+
+async function main() {
+  // 二重起動の防止
+  const gotTheLock = app.requestSingleInstanceLock()
+  if (!gotTheLock) {
+    app.quit()
+    return
+  }
+
+  const appHandlers = {
+    'activate': () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    },
+    'ready': async () => {
+      if (utls.isDevelopment && !process.env.IS_TEST) {
+        const installExtension = (await import('electron-devtools-installer'))
+          .default
+        const VUEJS_DEVTOOLS = (await import('electron-devtools-installer'))
+          .VUEJS_DEVTOOLS
+
+        // Install Vue Devtools
+        try {
+          await installExtension(VUEJS_DEVTOOLS)
+        } catch (e) {
+          console.error('Vue Devtools failed to install:', e.toString())
+        }
+      }
+    },
+    'window-all-closed': () => {
+      if (process.platform !== 'darwin') {
+        app.quit()
+      }
+    },
+  }
+  Object.entries(appHandlers).forEach(([eventName, handler]) =>
+    app.on(eventName, handler)
+  )
+
+  const Store = require('electron-store')
+  const store = new Store({
+    cwd: path.dirname(app.getPath('exe')),
+    defaults: {
+      position: {
+        x: 500,
+        y: 500,
+      },
+      size: {
+        width: 300,
+        height: 300,
+      },
+      text: 'abc',
+    },
+  })
+
+  await new Promise((resolve) => app.on('ready', resolve))
+
+  const position = store.get('position')
+  const size = store.get('size')
+  const maximized = store.get('maximized')
+  const minimized = store.get('minimized')
+  let beforeSize = [size.width, size.height]
+  let beforePosition = [position.x, position.y]
+  const options = {
+    x: position.x,
+    y: position.y,
+    width: size.width,
+    height: size.height,
+    minWidth: 300,
+    minHeight: 300,
+    transparent: true,
+    // transparent: false, // で最大化ができるようになる
+    frame: false,
+    toolbar: false,
+    hasShadow: false,
+    show: false,
+  }
+  const menuItems = [
+    {
+      label: 'devtool',
+      submenu: [
+        {
+          role: 'toggleDevTools',
+        },
+      ],
+    },
+  ]
+
+  const ipcHandlers = {
+    minimize: ({ sender }) => {
+      const win = sender.getOwnerBrowserWindow()
+      win.minimize()
+    },
+    reload: ({ sender }) => {
+      const win = sender.getOwnerBrowserWindow()
+      win.reload()
+    },
+    quit: () => app.quit(),
+    createWindow: (_, ...args) => utls.createWindow(...args),
+    getStore: (_, key) => store.get(key),
+    setStore: (_, key, value) => {
+      if (value === undefined) {
+        store.set(key)
+      } else {
+        store.set(key, value)
+      }
+    },
+    listen: ({ sender }, listenerName, eventName) => {
+      const win = sender.getOwnerBrowserWindow()
+      return utls.listenIpc(listenerName, eventName, (_, ...args) => {
+        return utls.sendIpc(win, listenerName, eventName, ...args)
+      })
+    },
+  }
+  Object.entries(ipcHandlers).forEach(([eventName, handler]) =>
+    utls.listenIpc('main', eventName, handler)
+  )
+
+  const mainWin = await utls.createWindow(options, menuItems, 'Main')
+  mainWin.showInactive()
+  if (maximized) {
+    mainWin.maximize()
+  } else if (minimized) {
+    mainWin.minimize()
+  }
+
+
+  mainWin.on('resized', () => {
+    if (mainWin.isMaximized()) {
+      return
+    }
+    beforeSize = mainWin.getSize()
+  })
+  mainWin.on('moved', () => {
+    if (mainWin.isMaximized()) {
+      return
+    }
+    beforePosition = mainWin.getPosition()
+  })
+  mainWin.on('close', () => {
+    const maximized = mainWin.isMaximized()
+    const position = maximized ? beforePosition : mainWin.getPosition()
+    const size = maximized ? beforeSize : mainWin.getSize()
+    const minimized = mainWin.isMinimized()
+    store.set({
+      position: {
+        x: position[0],
+        y: position[1],
+      },
+      size: {
+        width: size[0],
+        height: size[1],
+      },
+      maximized,
+      minimized,
+    })
+  })
+
+  // mainWin に送るのでこれだけ後で app.on する必要がある
+  app.on('second-instance', (_, rawArgs) => {
+    const args =
+      rawArgs?.[0] === 'electron.exe' ? rawArgs.slice(3) : rawArgs.slice(2)
+    utls.sendIpc(mainWin, 'main', 'commandline', ...args)
+  })
+
+  // Exit cleanly on request from parent process in development mode.
+  if (utls.isDevelopment) {
+    if (process.platform === 'win32') {
+      process.on('message', (data) => {
+        if (data === 'graceful-exit') {
+          app.quit()
+        }
+      })
+    } else {
+      process.on('SIGTERM', () => {
+        app.quit()
+      })
+    }
+  }
+}
+
+main()
+
---
+++ src/renderer/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-app>
+    <v-main>
+      <div class="electron-main">
+        <titlebar :title-left="titleLeft" :title-right="titleRight" />
+        <div class="electron-content">
+          <component
+            :is="components[name]"
+            v-model:title-left="titleLeft"
+            v-model:title-right="titleRight"
+          />
+        </div>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Titlebar from './components/titlebar.vue'
+import Main from './components/main.vue'
+import Sub from './components/sub.vue'
+const components = {
+  Main,
+  Sub,
+}
+
+export default {
+  name: 'App',
+  components: {
+    Titlebar,
+  },
+  data: () => ({
+    titleLeft: 'lefttttttttt',
+    titleRight: 'rightttttttt',
+  }),
+  computed: {
+    components() {
+      return components
+    },
+    name() {
+      return location.href.match(/(?<=#).+?$/)[0]
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+html {
+  overflow: hidden !important;
+  > body {
+    background-color: #ffffff00;
+    .v-application {
+      background-color: #ffffff00;
+      .v-main__wrap {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .electron-main {
+          height: calc(100vh - 4px);
+          width: calc(100vw - 4px);
+          font-family: Cica;
+          display: flex;
+          flex-direction: column;
+          .electron-content {
+            border-style: none solid solid;
+            border-color: lightgray;
+            border-width: 1px;
+            flex-grow: 1;
+            overflow: auto;
+          }
+          * {
+            &::-webkit-scrollbar-track {
+              background-color: #eee;
+            }
+            &::-webkit-scrollbar {
+              width: 10px;
+              height: 10px;
+            }
+            &::-webkit-scrollbar-thumb {
+              background-color: #ccc;
+              border-radius: 30px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
