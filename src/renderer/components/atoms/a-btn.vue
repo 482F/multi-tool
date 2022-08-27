489d94a0f93afffa2e4fe893f67064a3f08a4573
482F <482F.git.mail@482f.net>
2022-08-27 16:47:15 +0900

    タブ表示ができるように

---
+++ src/renderer/components/atoms/a-btn.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn class="a-btn" v-bind="$props" :style="{ '--bg-color': bgColor }">
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'a-btn',
+  props: {
+    variant: {
+      type: String,
+      default: 'outlined',
+    },
+    rounded: {
+      type: [String, Number],
+      default: 'primary',
+    },
+    bgColor: {
+      type: String,
+      default: 'white',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.a-btn {
+  background-color: var(--bg-color);
+}
+</style>
